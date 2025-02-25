84d2001bf5628bc6b428b5a37cebf81ee1cadd21
Catherine Alyssa <catherinealyssar@gmail.com>
2025-02-25 13:56:26 +0700

    feat: login scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import LoginPage from '@/pages/LoginPage';
 import AuthRedirect from '@/lib/utils/AuthRedirect';
 import ProtectedRoute from '@/lib/utils/ProtectedRoute';
 import { ChatPage } from '@/pages/(private)/chat';
+import { RouterProvider } from 'react-router';
+import { router } from '@/lib/router/router';
 
 
 
@@ -56,41 +58,42 @@ function App() {
   if (loading) return <p>Loading...</p>; // Prevent rendering while fetching user
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Navigate to={isAuthenticated ? '/temp' : '/login'} replace />
-          }
-        />
-        <Route element={<AuthRedirect isAuthenticated={isAuthenticated} />}>
-          <Route
-            path="/login"
-            element={<LoginPage setUsername={setUsername} />}
-          />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route
-            path="/chat"
-            element={<ChatPage setUsername={setUsername} />}
-          />
-        </Route>
-        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
-          <Route
-            path="/temp"
-            element={<ChatPage setUsername={setUsername} />}
-            // element={<TempPage username={username} setUsername={setUsername} />}
-            // element={<ChatPage />}
-          />
-        </Route>
-        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
-          <Route
-            path="/create"
-            element={<CreateLoanPostPage />}
-          />
-        </Route>
-      </Routes>
-    </Router>
+    <RouterProvider router={router}></RouterProvider>
+    // <Router>
+    //   <Routes>
+    //     <Route
+    //       path="/"
+    //       element={
+    //         <Navigate to={isAuthenticated ? '/temp' : '/login'} replace />
+    //       }
+    //     />
+    //     <Route element={<AuthRedirect isAuthenticated={isAuthenticated} />}>
+    //       <Route
+    //         path="/login"
+    //         element={<LoginPage setUsername={setUsername} />}
+    //       />
+    //       <Route path="/register" element={<RegisterPage />} />
+    //       <Route
+    //         path="/chat"
+    //         element={<ChatPage setUsername={setUsername} />}
+    //       />
+    //     </Route>
+    //     <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+    //       <Route
+    //         path="/temp"
+    //         element={<ChatPage setUsername={setUsername} />}
+    //         // element={<TempPage username={username} setUsername={setUsername} />}
+    //         // element={<ChatPage />}
+    //       />
+    //     </Route>
+    //     <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+    //       <Route
+    //         path="/create"
+    //         element={<CreateLoanPostPage />}
+    //       />
+    //     </Route>
+    //   </Routes>
+    // </Router>
   );
 }
 
