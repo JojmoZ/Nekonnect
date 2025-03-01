bb307bea007b494217468b6a3330e2f41bd8fc8c
Catherine Alyssa <catherinealyssar@gmail.com>
2025-03-01 14:33:37 +0700

    feat: logo component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,12 @@
+import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
+
 function Header() {
   return (
     <header>
-      <div>NeKonnect</div>
+      <Logo />
 
+      <Button variant={"gradient"}>Sign In</Button>
     </header>
   );
 }
