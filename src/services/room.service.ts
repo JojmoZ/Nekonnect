c7f495583d923c46ea0f2648ee1b4de7b93edecb
Steven Liementha <stevenliementha@gmail.com>
2025-03-04 01:52:43 +0700

    fix : fix anonymous caller create post

--- src/services/room.service.ts
+++ src/services/room.service.ts
@@ -2,8 +2,9 @@ import { roomDto } from "@/lib/model/dto/create-room.dto";
 import { BaseService, createRoomActor, roomCanisterId } from "./base.service";
 import { Room } from "@/lib/model/entity/room";
 import { Principal } from "@dfinity/principal";
-import { _SERVICE as _ROOMSERVICE } from "@/declarations/room/room.did";
+import { _SERVICE as _ROOMSERVICE, GetRoomsResponse } from "@/declarations/room/room.did";
 import { ActorSubclass } from "@dfinity/agent";
+import { GetRoomByPostIdResponse } from "@/lib/model/dto/response/get-room-by-post-id-response";
 
 export class RoomService extends BaseService {
 
@@ -20,15 +21,20 @@ export class RoomService extends BaseService {
     }
 
     async createRoom(roomDto : roomDto) {
-        return await this.room.createRoom( roomDto.room_name, roomDto.room_type);
+        return await this.room.createRoom( roomDto.room_name, roomDto.room_type, roomDto.post_id);
     }
 
-    async joinRoom(roomId : string,userId : Principal) {
-        return await this.room.join_room(roomId,userId);
+    async getRoomByPostId(postId : string) : Promise<GetRoomsResponse[]>  {
+        return await this.room.getRoomByPostId(postId);
     }
 
-    async createPrivateRoom(receiverId: Principal): Promise<string> {
-        const result = await this.room.createPrivateRoom(await this.getCallerPrincipal(), receiverId);
+    // async joinRoom(roomId : string,userId : Principal) {
+    //     return await this.room.join_room(roomId,userId);
+    // }
+
+    async createPrivateRoom(receiverId: Principal, post_id: string): Promise<string> {
+        console.log(post_id)
+        const result = await this.room.createPrivateRoom(await this.getCallerPrincipal(), receiverId,post_id);
 
         if ("ok" in result) {
             return result.ok; 
