0b749085713a4ba321011c3cd9def5ff8b179c14
Yenita Kimberly <yenitasiaoyen@gmail.com>
2025-03-05 14:02:29 +0700

    fix: time left calculation

--- src/hooks/loan-post/use-get-loan-posts.tsx
+++ src/hooks/loan-post/use-get-loan-posts.tsx
@@ -1,30 +1,33 @@
 import { LoanPost } from "@/lib/model/entity/loan-post";
-import { LoanPostService } from "@/services/loan-post.service";
 import { useEffect, useState } from "react";
-
-const loanPostService = new LoanPostService();
+import useServiceContext from "../use-service-context";
 
 export function useGetLoanPosts(active?: Boolean) {
   const [loanPosts, setLoanPosts] = useState<LoanPost[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const { loanPostService } = useServiceContext();
+
+  const fetchLoanPosts = async () => {
+    setLoading(true);
+    let response;
+    if (active === undefined) {
+      response = await loanPostService.getLoanPosts();
+    } else if (active) {
+      response = await loanPostService.getActivePosts();
+    } else {
+      response = await loanPostService.getUnverifiedPosts();
+    }
+
+    setLoanPosts(response);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const fetchLoanPosts = async () => {
-      let response;
-      if (active === undefined) {
-        response = await loanPostService.getLoanPosts();
-      } else if (active) {
-        response = await loanPostService.getActivePosts();
-      } else {
-        response = await loanPostService.getUnverifiedPosts();
-      }
-  
-      setLoanPosts(response);
-      setLoading(false);
-    };
-
     fetchLoanPosts();
-  }, [loanPosts]);
+    const interval = setInterval(fetchLoanPosts, 2000);
+
+    return () => clearInterval(interval);
+  }, [loanPostService]);
 
   return { loanPosts, getLoanPostsLoading: loading };
 }
