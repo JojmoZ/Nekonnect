6b719f6f26d1cb6d2c3bc281e31c982d7c0778f0
Cixilation <marvellavians6@gmail.com>
2025-03-05 18:48:27 +0700

    feat: brute force navigation bar

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,7 +8,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
+} from '@/components/ui/navigation-menu';
 import { ListItem } from '@/components/ui/list-item';
 import Link from 'next/link';
 import { useGetAuthenticated } from '@/hooks/user/use-get-authenticated';
@@ -18,41 +18,107 @@ import { RouteEnum } from '@/lib/enum/router-enum';
 
 const components: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+    title: 'Login',
+    href: RouteEnum.LOGIN,
+    description: 'Login to access your account.',
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
+    title: 'Home',
+    href: '/',
+    description: 'Main landing page.',
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    title: 'Create Post',
+    href: RouteEnum.CREATE_POST,
+    description: 'Create a new loan post.',
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
+    title: 'Edit Profile',
+    href: RouteEnum.EDIT_PROFILE,
+    description: 'Modify your profile details.',
   },
   {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    title: 'Chat',
+    href: '/chat',
+    description: 'Access your messages.',
   },
   {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    title: 'Post Verification',
+    href: RouteEnum.POST_VERIFICATION,
+    description: 'Verify posts before publishing.',
   },
-]
+  {
+    title: 'Loan Detail',
+    href: RouteEnum.POST,
+    description: 'View details of a loan post.',
+  },
+  {
+    title: 'Transaction History',
+    href: RouteEnum.TRANSACTION_HISTORY,
+    description: 'Check your past transactions.',
+  },
+  {
+    title: 'User Page',
+    href: '/temp',
+    description: 'Temporary user page.',
+  },
+  {
+    title: 'Temp Page 2',
+    href: '/temp2',
+    description: 'Another temporary page.',
+  },
+  {
+    title: 'For Borrowers',
+    href: RouteEnum.BORROWER,
+    description: 'Borrow money easily.',
+  },
+  {
+    title: 'Profile',
+    href: RouteEnum.PROFILE,
+    description: 'Manage your profile settings.',
+  },
+  {
+    title: 'Browse Loans',
+    href: RouteEnum.BROWSE,
+    description: 'Explore available loan posts.',
+  },
+];
+
+const for_borrowers: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Borrow',
+    href: RouteEnum.BORROWER,
+    description: 'Borrow money easily.',
+  },
+];
+
+const for_all: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Profile',
+    href: RouteEnum.PROFILE,
+    description: 'Manage your profile settings.',
+  },
+  {
+    title: 'Transaction History',
+    href: RouteEnum.TRANSACTION_HISTORY,
+    description: 'Check your past transactions.',
+  },
+];
+
+const for_lenders: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Browse Loans',
+    href: RouteEnum.BROWSE,
+    description: 'Explore available loan posts.',
+  },
+];
+const for_admin: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Verify Post',
+    href: RouteEnum.POST_VERIFICATION,
+    description: 'Verify posts before publishing.',
+  },
+];
 
 function Header() {
   const navigate = useNavigate();
@@ -60,33 +126,61 @@ function Header() {
   const { userService } = useServiceContext();
   const logout = () => {
     userService.logout();
-  }
+  };
 
   const redirect = () => {
     navigate('/login');
-  }
+  };
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-20 items-center">
         <Logo className="h-12" />
+
         <NavigationMenu className="ml-auto hidden gap-6 md:flex">
           <NavigationMenuList>
-            {/* TODO: reroute */}
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  For Borrowers
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href={RouteEnum.BROWSE} legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  For Lenders
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+            {[
+              {
+                label: 'For Borrowers',
+                links: for_borrowers,
+              },
+              {
+                label: 'For Lenders',
+                links: for_lenders,
+              },
+              {
+                label: 'Admin',
+                links: for_admin,
+              },
+              {
+                label: 'For All',
+                links: for_all,
+              },
+              {
+                label: 'All LINKS Available',
+                links: components,
+              },
+            ].map((menu) => (
+              <NavigationMenu>
+                <NavigationMenuItem key={menu.label}>
+                  <NavigationMenuTrigger>{menu.label}</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    {menu.links.map((route) => (
+                      <a key={route.href} href={route.href}>
+                        <div>
+                          <NavigationMenuLink
+                            className={navigationMenuTriggerStyle()}
+                            style={{ width: 200 }}
+                          >
+                            {route.title}
+                          </NavigationMenuLink>
+                        </div>
+                      </a>
+                    ))}
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenu>
+            ))}
             <NavigationMenuItem>
               <Link href="/docs" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -96,13 +190,17 @@ function Header() {
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
+
         <div className="ml-auto flex items-center gap-4 md:ml-4">
-          {
-            !isAuthenticated ?
-              <Button variant="gradient" onClick={redirect}>Sign In</Button>
-              :
-              <Button variant="gradient" onClick={logout}>Log out</Button>
-          }
+          {!isAuthenticated ? (
+            <Button variant="gradient" onClick={redirect}>
+              Sign In
+            </Button>
+          ) : (
+            <Button variant="gradient" onClick={logout}>
+              Log out
+            </Button>
+          )}
         </div>
       </div>
     </header>
