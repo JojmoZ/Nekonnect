e51cf24797115eca5dabd729db3d83f596c74c0e
JojmoZ <vincentiusjonathantanujaya@gmail.com>
2025-03-08 13:33:27 +0700

    fix:fix EOF

--- setup.sh
+++ setup.sh
@@ -32,4 +32,4 @@ win_path=$(wslpath -w ~/Nekonnect/ocr_backend)
 # Convert WSL path to Windows format
 cmd.exe /c start cmd /k "pushd $win_path && wsl --cd ~/Nekonnect/ic-websocket-gateway -- bash -c 'cargo run'"
 
-cmd.exe /c start cmd /k "pushd $win_path && wsl --cd ~/Nekonnect/ocr_backend -- bash -c 'pip install -r requirements.txt && python -m venv .venv && source .venv/bin/activate && python app.py'"
+cmd.exe /c start cmd /k "pushd $win_path && wsl --cd ~/Nekonnect/ocr_backend -- bash -c \"pip install -r requirements.txt && python -m venv .venv && source .venv/bin/activate && python app.py\""
