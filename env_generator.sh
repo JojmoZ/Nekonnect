d9f09e39acf0c336bd0b4a41948d331ab42bf344
Steven Lie <stevenliementha@gmail.com>
2025-03-08 22:40:07 +0700

    feat : add .env while not generated

--- env_generator.sh
+++ env_generator.sh
@@ -1,3 +1,4 @@
-echo -e "\nVITE_WS_GATEWAY_URL='ws://172.20.121.53:8080'" >> .env
-echo "VITE_IC_NETWORK_URL='http://127.0.0.1:4943'" >> .env
-echo "VITE_II_NETWORK='ic'" >> .env
+WS_IP=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+grep -qxF "VITE_WS_GATEWAY_URL='ws://$WS_IP:8080'" .env || echo "VITE_WS_GATEWAY_URL='ws://$WS_IP:8080'" >> .env
+grep -qxF "VITE_IC_NETWORK_URL='http://127.0.0.1:4943'" .env || echo "VITE_IC_NETWORK_URL='http://127.0.0.1:4943'" >> .env
+grep -qxF "VITE_II_NETWORK='ic'" .env || echo "VITE_II_NETWORK='ic'" >> .env
